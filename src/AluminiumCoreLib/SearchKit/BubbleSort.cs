9f0c721be44f878db01c52eb7ae0db5597b87492
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2019-07-17 17:18:56 +0200

    move to SearchKit

--- src/AluminiumCoreLib/SearchKit/BubbleSort.cs
+++ src/AluminiumCoreLib/SearchKit/BubbleSort.cs
@@ -21,12 +21,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
     */
-using AluminiumCoreLib.Utilities;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace AluminiumCoreLib.SearchAndSort{
+namespace AluminiumCoreLib.SearchKit
+{
     /// <summary>
     /// 
     /// </summary>
