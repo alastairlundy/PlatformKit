902cb5263940418f38f32d04d86e5fd3e9653968
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2020-06-11 04:18:46 +0100

    Create the LinuxPlatform class to handle detecting things Linux users and devs may need

---
+++ src/LinuxPlatform.cs
@@ -0,0 +1,124 @@
+/* MIT License
+
+Copyright (c) 2018-2020 AluminiumTech
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+    */
+
+using System;
+using System.IO;
+
+using System.Runtime.InteropServices;
+
+namespace AluminiumTech.PlatformKit
+{
+    public class LinuxPlatform
+    {
+        protected ProcessManager _processManager;
+
+        public LinuxPlatform()
+        {
+            _processManager = new ProcessManager();
+        }
+        
+        /// <summary>
+        /// Returns the Linux Desktop Environment used by a Distro.
+        /// </summary>
+        /// <returns></returns>
+        public string GetLinuxDesktopEnvironment()
+        {
+            _processManager.RunProcessLinux("echo $XDG_CURRENT_DESKTOP");
+            
+            TextReader reader = Console.In;
+            return reader.ReadLine();
+        }
+        
+        /// <summary>
+        /// Gets all the information about the Linux Kernel returned as a String.
+        /// </summary>
+        /// <returns></returns>
+        public string GetLinuxKernelInformation()
+        {
+            return GetUnameInformation("--all");
+        }
+
+        /// <summary>
+        /// Returns the Linux Kernel Version as reported by "uname -r"
+        /// </summary>
+        /// <returns></returns>
+        public string GetLinuxKernelVersionToString()
+        {
+            return GetUnameInformation("-r");
+        }
+
+        /// <summary>
+        /// Returns the name of the Linux Distribution as reported by "lsb_release --id"
+        /// </summary>
+        /// <returns></returns>
+        public string GetDistributionName()
+        {
+            return GetLsb_releaseInformation("--id").Replace("Distributor ID: ", " ");
+        }
+        
+        /// <summary>
+        /// Returns the "Release" of the Distribution (what we call the Version)
+        /// </summary>
+        /// <returns></returns>
+        public string GetDistributionVersion()
+        {
+            return GetLsb_releaseInformation("--release").Replace("Release: ", " ");
+        }
+        
+        /// <summary>
+        /// Returns the description of the Linux Distribution
+        /// </summary>
+        /// <returns></returns>
+        public string GetDistributionDescription()
+        {
+            return GetLsb_releaseInformation("--description").Replace("Description: ", " ");
+        }
+
+        /// <summary>
+        /// Returns the output of running "lsb_release" with a specified parameter
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public string GetLsb_releaseInformation(string parameter)
+        {
+            _processManager.RunProcessLinux("lsb_release " + parameter);
+            
+            TextReader reader = Console.In;
+           return reader.ReadLine();
+        }
+        
+        /// <summary>
+        /// Returns the output of running "uname" with a specified parameter
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public string GetUnameInformation(string parameter)
+        {
+            _processManager.RunProcessLinux("uname " + parameter);
+            
+            TextReader reader = Console.In;
+            return reader.ReadLine();
+        }
+
+    }
+}
