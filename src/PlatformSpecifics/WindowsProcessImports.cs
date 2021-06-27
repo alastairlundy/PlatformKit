0be1794ca512732f775c399311d4fa9cead867b4
AluminiumTech <contact@aluminiumte.ch>
2021-06-27 03:32:16 +0100

    mention CC BY SA 3.0 code

---
+++ src/PlatformSpecifics/WindowsProcessImports.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * https://stackoverflow.com/questions/71257/suspend-process-in-c-sharp
+ * 
+ * Code licensed under CC BY SA 3.0 from StackOverflow.
+ * 
+ */
+using AluminiumTech.DevKit.PlatformKit.enums;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace AluminiumTech.DevKit.PlatformKit.PlatformSpecifics
+{
+    internal class WindowsProcessImports
+    {
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenThread(WindowsThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+
+
+    }
+}
---
+++ src/PlatformSpecifics/WindowsProcessSpecifics.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * https://stackoverflow.com/questions/71257/suspend-process-in-c-sharp
+ * 
+ * Code licensed under CC BY SA 3.0 from StackOverflow.
+ * 
+ */
+
+using AluminiumTech.DevKit.PlatformKit.enums;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace AluminiumTech.DevKit.PlatformKit.PlatformSpecifics{
+    public class WindowsProcessSpecifics{
+        public static void Suspend(Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = WindowsProcessImports.OpenThread(WindowsThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                WindowsProcessImports.SuspendThread(pOpenThread);
+            }
+        }
+        public static void Resume(Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = WindowsProcessImports.OpenThread(WindowsThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                WindowsProcessImports.ResumeThread(pOpenThread);
+            }
+        }
+    }
+}
