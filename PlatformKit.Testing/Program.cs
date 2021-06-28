6ad7f7f593fe240c704ace912e0e4c5fac14b89c
AluminiumTech <contact@aluminiumte.ch>
2021-06-28 01:49:26 +0100

    added testing program

---
+++ PlatformKit.Testing/Program.cs
@@ -0,0 +1,34 @@
+﻿using AluminiumTech.DevKit.PlatformKit;
+using System;
+using System.Diagnostics;
+
+namespace PlatformKit.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Platform platform = new Platform();
+            ProcessManager processManager = new ProcessManager();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            processManager.SuspendProcess("Spotify");
+
+            Console.WriteLine("Start suspending...");
+
+        //    PerformanceCounter
+
+            while (stopwatch.ElapsedMilliseconds < (1 * 1000))
+            {
+
+            }
+            processManager.ResumeProcess("Spotify");
+
+            Console.WriteLine("Start resuming...");
+
+            Console.ReadLine();
+        }
+    }
+}
