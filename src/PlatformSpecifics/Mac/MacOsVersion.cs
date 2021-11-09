54f00016cd46ab1523694c10b298e6478043172e
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2021-11-09 02:00:10 +0000

    add macOS enums

---
+++ src/PlatformSpecifics/Mac/MacOsVersion.cs
@@ -0,0 +1,48 @@
+/* MIT License
+
+Copyright (c) 2018-2021 AluminiumTech
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+// ReSharper disable All
+namespace AluminiumTech.DevKit.PlatformKit.PlatformSpecifics.Mac
+{
+    public enum MacOsVersion
+    {
+        v10_0_Cheetah,
+        v10_1_Puma,
+        v10_2_Jaguar,
+        v10_3_Panther,
+        v10_4_Tiger,
+        v10_5_Leopard,
+        v10_6_SnowLeopard,
+        v10_7_Lion,
+        v10_8_MountainLion,
+        v10_9_Mavericks,
+        v10_10_Yosemite,
+        v10_11_ElCapitan,
+        v10_12_Sierra,
+        v10_13_HighSierra,
+        v10_14_Mojave,
+        v10_15_Catalina,
+        v11_BigSur,
+        v12_Monterrey
+    }
+}
