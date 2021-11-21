bea3ea2f10d730a7893c9f6256294b5a3d13fe2e
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2021-11-21 16:52:30 +0000

    fixed an issue

--- src/Extensions/ProcessExtensions.cs
+++ src/Extensions/ProcessExtensions.cs
@@ -25,8 +25,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
-using AluminiumTech.DevKit.PlatformKit.Deprecation.Deprecated;
-
 using AluminiumTech.DevKit.PlatformKit.PlatformSpecifics.Windows;
 
 namespace AluminiumTech.DevKit.PlatformKit.Extensions
