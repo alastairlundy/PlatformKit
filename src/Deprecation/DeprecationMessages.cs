022c55caa845ca4d57fc1447985bbef6d3b25522
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2021-12-15 14:15:47 +0000

    update messages

--- src/Deprecation/DeprecationMessages.cs
+++ src/Deprecation/DeprecationMessages.cs
@@ -25,23 +25,32 @@ namespace AluminiumTech.DevKit.PlatformKit.Deprecation
 {
     public static class DeprecationMessages
     {
-        private const string V2_3 = "v2.3.0";
-        private const string V2_4 = "v2.4.0";
         private const string V2_5 = "v2.5.0";
+        private const string V2_6 = "v2.6.0";
+        private const string V2_7 = "v2.7.0";
+        private const string V2_8 = "v2.8.0";
+        private const string V2_9 = "v2.9.0";
+        private const string V2_10 = "v2.10.0";
         
         private const string V3 = "v3.0.0";
         private const string V4 = "v4.0.0";
+        private const string V5 = "v5.0.0";
 
         private const string CodeDeprecation = "This code is deprecated and will be removed";
         private const string FeatureDeprecation = "This feature is deprecated and will be removed";
         
         public const string FutureDeprecation = CodeDeprecation + " in a future version.";
 
-        public const string DeprecationV2_3 = CodeDeprecation + " in " + V2_3;
-        public const string DeprecationV2_4 = CodeDeprecation + " in " + V2_4;
         public const string DeprecationV2_5 = CodeDeprecation + " in " + V2_5;
+        
+        public const string DeprecationV2_6 = CodeDeprecation + " in " + V2_6;
+        public const string DeprecationV2_7 = CodeDeprecation + " in " + V2_7;
+        public const string DeprecationV2_8 = CodeDeprecation + " in " + V2_8;
+        public const string DeprecationV2_9 = CodeDeprecation + " in " + V2_9;
+        public const string DeprecationV2_10 = CodeDeprecation + " in " + V2_10;
         
         public const string DeprecationV3 = CodeDeprecation + " in " + V3;
         public const string DeprecationV4 = CodeDeprecation + " in " + V4;
+        public const string DeprecationV5 = CodeDeprecation + " in " + V5;
     }
 }
