55d0d524f199cdfef3d565556d2cdb6078240a24
AluminiumTech <293557-AluminiumTech@users.noreply.gitlab.com>
2022-02-17 00:31:07 +0000

    update namespace

--- src/Hardware/Shared/Models/ReleaseYearTimeframe.cs
+++ src/Hardware/Shared/Models/ReleaseYearTimeframe.cs
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-namespace AluminiumTech.DevKit.PlatformKit.Hardware.Shared;
+namespace AluminiumTech.DevKit.PlatformKit.Hardware.Shared.Models;
 
 public enum ReleaseYearTimeframe
 {
