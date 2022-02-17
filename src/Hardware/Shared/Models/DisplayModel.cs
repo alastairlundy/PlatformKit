96824ebc1a290ad7c47c771a653294272ff97192
AluminiumTech <293557-AluminiumTech@users.noreply.gitlab.com>
2022-02-17 00:33:19 +0000

    add new mac models

---
+++ src/Hardware/Shared/Models/DisplayModel.cs
@@ -0,0 +1,10 @@
+namespace AluminiumTech.DevKit.PlatformKit.Hardware.Shared.Models;
+
+public class DisplayModel
+{
+    public float DisplaySizeDiagonalInches { get; set; }
+    
+    public int HorizontalResolution { get; set; }
+    
+    public int VerticalResolution { get; set; }
+}
