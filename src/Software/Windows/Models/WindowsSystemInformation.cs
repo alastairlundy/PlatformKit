c9ad8f07e4129c50d18629f9a0c9e1fa87a4d62a
AluminiumTech <293557-AluminiumTech@users.noreply.gitlab.com>
2022-04-11 21:27:32 +0100

    add model for holding Windows systeminfo

---
+++ src/Software/Windows/Models/WindowsSystemInformation.cs
@@ -0,0 +1,79 @@
+﻿/* MIT License
+
+Copyright (c) 2018-2022 AluminiumTech
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
+// ReSharper disable InconsistentNaming
+
+namespace AluminiumTech.DevKit.PlatformKit.Software.Windows.Models;
+
+/// <summary>
+/// 
+/// </summary>
+public class WindowsSystemInformation
+{
+    public string HostName { get; set; }
+    
+    public string OsName { get; set; }
+    public string OsVersion { get; set; }
+    public string OsManufacturer { get; set; }
+    public string OsConfiguration { get; set; }
+    public string OsBuildType { get; set; }
+    
+    public string RegisteredOwner { get; set; }
+    public string RegisteredOrganization { get; set; }
+    
+    public string ProductId { get; set; }
+    
+    public DateTime OriginalInstallDate { get; set; }
+    public DateTime SystemBootTime { get; set; }
+    
+    public string SystemManufacturer { get; set; }
+    public string SystemModel { get; set; }
+    public string SystemType { get; set; }
+    
+    public string[] Processors { get; set; }
+    
+    public string BiosVersion { get; set; }
+    
+    public string SystemLocale { get; set; }
+    public string InputLocale { get; set; }
+    
+    public TimeZoneInfo TimeZone { get; set; }
+    
+    public int TotalPhysicalMemoryMB { get; set; }
+    public int AvailablePhysicalMemoryMB { get; set; }
+    
+    public int VirtualMemoryMaxSizeMB { get; set; }
+    public int VirtualMemoryAvailableSizeMB { get; set; }
+    public int VirtualMemoryInUse { get; set; }
+    
+    public string[] PageFileLocations { get; set; }
+    
+    public string LogonServer { get; set; }
+    
+    public string HotfixesInstalled { get; set; }
+    
+    public string[] NetworkCards { get; set; }
+    
+    public string[] HyperVRequirements { get; set; }
+}
