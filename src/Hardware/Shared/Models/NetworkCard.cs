831785add5269c9d47aaecb04ade53283b9918f7
AluminiumTech <293557-AluminiumTech@users.noreply.gitlab.com>
2022-05-06 23:41:13 +0100

    add models required for representing network cards

---
+++ src/Hardware/Shared/Models/NetworkCard.cs
@@ -0,0 +1,14 @@
+﻿namespace AluminiumTech.DevKit.PlatformKit.Hardware.Shared.Models;
+
+public class NetworkCard
+{
+    public string Name { get; set; }
+    
+    public string ConnectionName { get; set; }
+    
+    public bool DhcpEnabled { get; set; }
+    
+    public string DhcpServer { get; set; }
+
+    public string[] IpAddresses { get; set; }
+}
