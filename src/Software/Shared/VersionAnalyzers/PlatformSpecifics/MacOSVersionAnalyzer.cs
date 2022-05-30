37bd54963bbe8023390635772dc7c2f130fbe6aa
AluminiumTech <293557-AluminiumTech@users.noreply.gitlab.com>
2022-05-30 23:38:50 +0100

    fix macOS detection issues

--- src/Software/Shared/VersionAnalyzers/PlatformSpecifics/MacOSVersionAnalyzer.cs
+++ src/Software/Shared/VersionAnalyzers/PlatformSpecifics/MacOSVersionAnalyzer.cs
@@ -305,7 +305,7 @@ public static class MacOSVersionAnalyzer
     /// <returns></returns>
     public static Version DetectMacOsVersion(this OSVersionAnalyzer osVersionAnalyzer)
     {
-        var version = GetMacSwVersInfo()[1].ToLower().Replace("ProductVersion", String.Empty).Replace(" ", String.Empty);
+        var version = GetMacSwVersInfo()[1].Replace("ProductVersion:", String.Empty).Replace(" ", String.Empty);
 
         int dotCounter = 0;
 
@@ -346,6 +346,6 @@ public static class MacOSVersionAnalyzer
     private static string[] GetMacSwVersInfo()
     {
         // ReSharper disable once StringLiteralTypo
-        return new ProcessManager().RunMacCommand("sw_vers").Split(' ');
+        return new ProcessManager().RunMacCommand("sw_vers").Split(Convert.ToChar(Environment.NewLine));
     }
 }
