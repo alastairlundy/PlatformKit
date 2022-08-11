dd37e245d56acd2d89ccd72a5a5d2fb593e1cc28
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2022-08-11 17:04:30 +0200

    fixed issue in getting Windows System Info

--- src/Platforms/Windows/WindowsAnalyzer.cs
+++ src/Platforms/Windows/WindowsAnalyzer.cs
@@ -52,13 +52,7 @@ public class WindowsAnalyzer
         {
             if (OSAnalyzer.IsWindows())
             {
-                var desc = _processManager.RunPowerShellCommand("systeminfo");
-
-                desc = desc.Replace("  ", String.Empty);
-                
-                var arr = desc.Replace(":", String.Empty).Split(' ');
-                
-                var edition = arr[41].Replace("OS", String.Empty);
+                var edition = GetWindowsSystemInformation().OsName.ToLower();
 
                 //var edition = GetWMIValue("Name", "Win32_OperatingSystem");
 
