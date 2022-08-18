4311f99dde933c84aede1b6426f6dfdf9dcd829c
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2022-08-18 14:39:29 +0200

    Fix XNU detection on Apple Silicon macs

--- src/Software/Shared/VersionAnalyzers/PlatformSpecifics/MacOSVersionAnalyzer.cs
+++ src/Software/Shared/VersionAnalyzers/PlatformSpecifics/MacOSVersionAnalyzer.cs
@@ -195,11 +195,21 @@ public static class MacOSVersionAnalyzer
                 if (arr[index].ToLower().StartsWith("root:xnu-"))
                 {
                     arr[index] = arr[index].Replace("root:xnu-", String.Empty)
-                        .Replace("/RELEASE_X86_64", String.Empty)
                         .Replace("~", ".");
 
-                    //Console.WriteLine(arr[index]);
-                
+                    if (new OSAnalyzer().IsAppleSiliconMac())
+                    {
+                        arr[index] = arr[index].Replace("/RELEASE_ARM64_T8101", String.Empty);
+                    }
+                    else
+                    {
+                        arr[index] = arr[index].Replace("/RELEASE_X86_64", String.Empty);
+                    }
+
+            #if DEBUG
+                    Console.WriteLine(arr[index]);
+            #endif                
+
                     return Version.Parse(arr[index]);
                 }
             }
