eb9d3e614e3600b02e03d5d094c6d7bcfc3e65dd
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2022-10-28 12:28:05 +0100

    simplify detection by moving code to base class

--- src/Hardware/Common/Components/Detection/MemoryDetection.cs
+++ src/Hardware/Common/Components/Detection/MemoryDetection.cs
@@ -23,15 +23,19 @@ using PlatformKit.Windows;
 
 namespace PlatformKit.Hardware.Common;
 
-public class MemoryDetection
+public class MemoryDetection : BaseDetection
 {
-    protected MemoryModel DetectWindows()
+    public MemoryDetection()
+    {
+        _windowsAnalyzer = new WindowsAnalyzer();
+        _macOsAnalyzer = new MacOSAnalyzer();
+    }
+    
+    protected override MemoryModel DetectWindows()
     {
         MemoryModel memoryModel = new MemoryModel();
-
-        WindowsAnalyzer windowsAnalyzer = new WindowsAnalyzer();
-
-        var windowsSystemInfo = windowsAnalyzer.GetWindowsSystemInformation();
+        
+        var windowsSystemInfo = _windowsAnalyzer.GetWindowsSystemInformation();
         
         memoryModel.TotalPhysicalRamMB = windowsSystemInfo.TotalPhysicalMemoryMB;
         memoryModel.AvailablePhysicalRamMB = windowsSystemInfo.AvailablePhysicalMemoryMB;
@@ -39,45 +43,20 @@ public class MemoryDetection
         memoryModel.TotalVirtualRamMB = windowsSystemInfo.VirtualMemoryMaxSizeMB;
 
        //Get all other MemoryModel values from WMI
-       memoryModel.MinVoltageMillivolts = int.Parse(windowsAnalyzer.GetWMIValue("MinVoltage", "Win32_PhysicalMemory"));
-       memoryModel.MaxVoltageMillivolts = int.Parse(windowsAnalyzer.GetWMIValue("MaxVoltage", "Win32_PhysicalMemory"));
-       memoryModel.MemorySpeedMHz = int.Parse(windowsAnalyzer.GetWMIValue("ConfiguredClockSpeed", "Win32_PhysicalMemory"));
+       memoryModel.MinVoltageMillivolts = int.Parse(_windowsAnalyzer.GetWMIValue("MinVoltage", "Win32_PhysicalMemory"));
+       memoryModel.MaxVoltageMillivolts = int.Parse(_windowsAnalyzer.GetWMIValue("MaxVoltage", "Win32_PhysicalMemory"));
+       memoryModel.MemorySpeedMHz = int.Parse(_windowsAnalyzer.GetWMIValue("ConfiguredClockSpeed", "Win32_PhysicalMemory"));
 
        return memoryModel;
     }
 
-    protected MemoryModel DetectMac()
+    protected override MemoryModel DetectMac()
     {
         MemoryModel memoryModel = new MemoryModel();
 
-        MacOSAnalyzer macOsAnalyzer = new MacOSAnalyzer();
-
-      //  memoryModel.AvailablePhysicalRamMB =
+        //  memoryModel.AvailablePhysicalRamMB =
         //    macOsAnalyzer.GetMacSystemProfilerInformation(MacSystemProfilerDataType.HardwareDataType, "");
 
         return memoryModel;
-    }
-
-    protected MemoryModel DetectLinux()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public MemoryModel Detect()
-    {
-        if (OSAnalyzer.IsWindows())
-        {
-            return DetectWindows();
-        }
-        else if (OSAnalyzer.IsMac())
-        {
-            return DetectMac();
-        }
-        else if (OSAnalyzer.IsLinux())
-        {
-            return DetectLinux();
-        }
-
-        throw new PlatformNotSupportedException();
     }
 }
--- src/Hardware/Common/Components/Detection/ProcessorDetection.cs
+++ src/Hardware/Common/Components/Detection/ProcessorDetection.cs
@@ -19,9 +19,9 @@ using PlatformKit.Windows;
 
 namespace PlatformKit.Hardware.Common;
 
-public class ProcessorDetection
+public class ProcessorDetection : BaseDetection
 {
-    protected ProcessorModel DetectWindows()
+    protected override ProcessorModel DetectWindows()
     {
         ProcessorModel processorModel = new ProcessorModel();
 
@@ -31,7 +31,7 @@ public class ProcessorDetection
         processorModel.CoreCount = int.Parse(windowsAnalyzer.GetWMIValue("NumberOfCores", "Win32_Processor"));
         processorModel.ThreadCount = int.Parse(windowsAnalyzer.GetWMIValue("ThreadCount", "Win32_Processor"));
         //processorModel Nominal clockspeed
-        
+
         processorModel.BoostClockSpeedMHz = int.Parse(windowsAnalyzer.GetWMIValue("MaxClockSpeed", "Win32_Processor"));
 
         processorModel.ArchitectureName = windowsAnalyzer.GetWMIValue("Architecture", "Win32_Processor");
@@ -43,39 +43,10 @@ public class ProcessorDetection
 
         processorModel.Socket = windowsAnalyzer.GetWMIValue("SocketDesignation", "Win32_Processor");
         processorModel.Revision = windowsAnalyzer.GetWMIValue("Revision", "Win32_Processor");
-        
-        processorModel.SupportsVirtualization = Boolean.Parse(windowsAnalyzer.GetWMIValue("VirtualizationFirmwareEnabled", "Win32_Processor"));
+
+        processorModel.SupportsVirtualization =
+            Boolean.Parse(windowsAnalyzer.GetWMIValue("VirtualizationFirmwareEnabled", "Win32_Processor"));
 
         return processorModel;
     }
-
-    protected ProcessorModel DetectMac()
-    {
-        throw new NotImplementedException();
-    }
-
-    protected ProcessorModel DetectLinux()
-    {
-        throw new NotImplementedException();
-    }
-
-    public ProcessorModel Detect()
-    {
-        if (OSAnalyzer.IsWindows())
-        {
-            return DetectWindows();
-        }
-        else if (OSAnalyzer.IsMac())
-        {
-            return DetectMac();
-        }
-        else if (OSAnalyzer.IsLinux())
-        {
-            return DetectLinux();
-        }
-
-        throw new PlatformNotSupportedException();
-    }
-    
-    
 }
