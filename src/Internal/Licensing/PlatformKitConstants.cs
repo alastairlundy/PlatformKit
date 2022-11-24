ace8c46135129d2c27692663b950d07c0bf668d3
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2022-11-24 14:02:15 +0000

    remove Non-Commercial License

--- src/Internal/Licensing/PlatformKitConstants.cs
+++ src/Internal/Licensing/PlatformKitConstants.cs
@@ -4,11 +4,9 @@
  You may choose to use PlatformKit under either license so long as you abide by the respective license's terms and restrictions.
  
  You can view the GPLv3 in the file GPLv3_License.md .
- You can view the PlatformKit Licenses at
-    Commercial License - https://neverspy.tech/platformkit-commercial-license or in the file PlatformKit_Commercial_License.txt
-    Non-Commercial License - https://neverspy.tech/platformkit-noncommercial-license or in the file PlatformKit_NonCommercial_License.txt
-  
-  To use PlatformKit under either a commercial or non-commercial license you must purchase a license from https://neverspy.tech
+ You can view the PlatformKit Licenses at https://neverspy.tech/platformkit-commercial-license or in the file PlatformKit_Commercial_License.txt
+
+ To use PlatformKit under a commercial license you must purchase a license from https://neverspy.tech
  
  Copyright (c) AluminiumTech 2018-2022
  Copyright (c) NeverSpy Tech Limited 2022
@@ -35,24 +33,24 @@ public class PlatformKitConstants
             false,
             false,
             true));
-    
-    public static readonly ProprietaryLicense PlatformKitNonCommercialLicense = new("PlatformKit Non-Commercial License",
-        LicenseType.ClosedSourceProprietary,
-        new LicensePermissionsModel(true,
-            false,
-            true,
-            true,
-            false,
-            false,
-            false,
-            true));
 
     internal static void CheckLicenseState()
     {
-        LicenseManager.RegisterLicense(PlatformKitConstants.GPLv3_OrLater);
-        LicenseManager.RegisterLicense(PlatformKitConstants.PlatformKitCommercialLicense);
-        LicenseManager.RegisterLicense(PlatformKitNonCommercialLicense);
+        LicenseManager.RegisterLicense(GPLv3_OrLater);
+        LicenseManager.RegisterLicense(PlatformKitCommercialLicense);
         LicenseManager.SelectedLicense = PlatformKitSettings.SelectedLicense;
         LicenseManager.CheckLicenseStatus();
+
+        if (LicenseManager.LicenseUsageState == LicenseUsageState.AcceptableLicenseSelected)
+        {
+            if (PlatformKitSettings.SelectedLicense == PlatformKitCommercialLicense){
+               /*
+                *  if (PlatformKitSettings.LicenseKey == null)
+                {
+                    LicenseManager.LicenseUsageState = 
+                }
+                */
+            }
+        }
     }
 }
