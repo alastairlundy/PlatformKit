60d9617b43e8006d984e120a3a2bf30b83937c4a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-12-18 22:52:22 +0000

    add enum required for Linux DistroBase detection

---
+++ PlatformKit/Platforms/Linux/Enums/LinuxDistroBase.cs
@@ -0,0 +1,19 @@
+/*
+      PlatformKit
+      
+      Copyright (c) Alastair Lundy 2018-2023
+      
+      This Source Code Form is subject to the terms of the Mozilla Public
+      License, v. 2.0. If a copy of the MPL was not distributed with this
+      file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+namespace PlatformKit.Linux.Enums;
+
+public enum LinuxDistroBase
+{
+    Debian,
+    Ubuntu,
+    Arch,
+    Fedora
+}
