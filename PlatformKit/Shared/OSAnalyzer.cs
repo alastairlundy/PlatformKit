666cd1764eb683f5e9e5ab8f4a82890bd80e9c1c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-01-16 20:59:07 +0000

    remove GetOSPlatform

--- PlatformKit/Shared/OSAnalyzer.cs
+++ PlatformKit/Shared/OSAnalyzer.cs
@@ -79,30 +79,6 @@ namespace PlatformKit
         }
         
         /// <summary>
-        /// Determine what OS is being run
-        /// </summary>
-        /// <returns></returns>
-        // ReSharper disable once UnusedMember.Global
-        // ReSharper disable once InconsistentNaming
-        [Obsolete(DeprecationMessages.FutureDeprecation)]
-        public static System.Runtime.InteropServices.OSPlatform GetOSPlatform() {
-            System.Runtime.InteropServices.OSPlatform osPlatform = System.Runtime.InteropServices.OSPlatform.Create("Other Platform");
-            // Check if it's windows
-            osPlatform = IsWindows() ? System.Runtime.InteropServices.OSPlatform.Windows : osPlatform;
-            // Check if it's osx
-            osPlatform = IsMac() ? System.Runtime.InteropServices.OSPlatform.OSX : osPlatform;
-            // Check if it's Linux
-            osPlatform = IsLinux() ? System.Runtime.InteropServices.OSPlatform.Linux : osPlatform;
-            
-#if NETCOREAPP3_0_OR_GREATER
-            // Check if it's FreeBSD
-            osPlatform = IsFreeBSD() ? System.Runtime.InteropServices.OSPlatform.FreeBSD : osPlatform;
-#endif
-
-            return osPlatform;
-        }
-        
-        /// <summary>
         /// Detect the OS version on Windows, macOS, or Linux.
         /// </summary>
         /// <returns></returns>
