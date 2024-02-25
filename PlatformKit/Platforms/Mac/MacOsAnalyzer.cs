4bcb2aeda36036a778967e3380647097d0d110fc
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-02-25 23:46:16 +0000

    update copyright notices to 2024

--- PlatformKit/Platforms/Mac/MacOsAnalyzer.cs
+++ PlatformKit/Platforms/Mac/MacOsAnalyzer.cs
@@ -1,7 +1,7 @@
 /*
     PlatformKit
     
-    Copyright (c) Alastair Lundy 2018-2023
+    Copyright (c) Alastair Lundy 2018-2024
     
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -9,7 +9,6 @@
  */
 
 using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using PlatformKit.Internal.Deprecation;
 using PlatformKit.Internal.Exceptions;
