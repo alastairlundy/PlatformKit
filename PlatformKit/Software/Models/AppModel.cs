245a785095e68070fb7f13912f1a4740c525036b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-21 11:56:52 +0000

    add AppModel to prepare for future Software functionality

---
+++ PlatformKit/Software/Models/AppModel.cs
@@ -0,0 +1,24 @@
+/*
+      PlatformKit
+      
+      Copyright (c) Alastair Lundy 2018-2024
+      
+      This Source Code Form is subject to the terms of the Mozilla Public
+      License, v. 2.0. If a copy of the MPL was not distributed with this
+      file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+#nullable enable
+using System;
+
+namespace PlatformKit.Software;
+
+public class AppModel
+{
+    public string? Author { get; set; }
+    
+    public string? FriendlyName { get; set; }
+    public string ExecutableName { get; set; }
+    public string InstallLocation { get; set; }
+    public Version? InstalledVersion { get; set; }
+}
