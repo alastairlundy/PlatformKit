127d99625ed4442d599daaf564a0ef390fe0a24e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-22 23:03:40 +0000

    add Platform enum

---
+++ PlatformKit/Shared/Enums/Platforms.cs
@@ -0,0 +1,25 @@
+﻿namespace PlatformKit.Enums;
+
+public enum Platform
+{
+    // ReSharper disable once InconsistentNaming
+    IOS,
+    Android,
+    Windows,
+    // ReSharper disable once InconsistentNaming
+    macOs,
+    Linux,
+    FreeBsd,
+    Fuchsia,
+    // ReSharper disable once InconsistentNaming
+    FireOS,
+    // ReSharper disable once InconsistentNaming
+    tvOS,
+    // ReSharper disable once InconsistentNaming
+    watchOS,
+    wearOS,
+    // ReSharper disable once InconsistentNaming
+    visionOS,
+    Browser,
+    
+}
