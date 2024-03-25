02d9a1146ad24c52c0b2ab5c4649770b699b7451
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-25 20:38:15 +0000

    update exception message

--- PlatformKit/Internal/Exceptions/OperatingSystemDetectionException.cs
+++ PlatformKit/Internal/Exceptions/OperatingSystemDetectionException.cs
@@ -15,7 +15,7 @@ namespace PlatformKit.Internal.Exceptions
 {
     internal class OperatingSystemDetectionException : Exception
     {
-        internal OperatingSystemDetectionException() : base("Failed to detect details about the Operating System running.")
+        internal OperatingSystemDetectionException() : base("Failed to detect details about the Operating System running on this device.")
         {
             
         }
