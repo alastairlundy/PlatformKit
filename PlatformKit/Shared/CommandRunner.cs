651b84498ff4dc88e8df98d8d8be17bd6fce3c9b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-03-25 18:24:58 +0000

    move CommandRunner to PlatformKit namespace

--- PlatformKit/Shared/CommandRunner.cs
+++ PlatformKit/Shared/CommandRunner.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿/*
+      PlatformKit
+      
+      Copyright (c) Alastair Lundy 2018-2024
+      
+      This Source Code Form is subject to the terms of the Mozilla Public
+      License, v. 2.0. If a copy of the MPL was not distributed with this
+      file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+using System;
 using System.Diagnostics;
 using System.IO;
-using PlatformKit.Extensions;
-using PlatformKit.Linux;
 
-namespace PlatformKit.Software;
+namespace PlatformKit;
 
 /// <summary>
 /// 
