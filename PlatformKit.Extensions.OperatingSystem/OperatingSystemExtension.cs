aa898461685a94883dff4795576db5235d452ecc
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-04-10 18:41:39 +0100

    rename extension class and add methods to allow using static extension methods

--- PlatformKit.Extensions.OperatingSystem/OperatingSystemExtension.cs
+++ PlatformKit.Extensions.OperatingSystem/OperatingSystemExtension.cs
@@ -1,40 +1,55 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace PlatformKit.Classic.OSDetection
+namespace PlatformKit.Extensions.OperatingSystem
 {
     public static class OperatingSystemExtension
     {
-        public static bool IsWindows(this OperatingSystem operatingSystem)
+        internal static System.OperatingSystem GetSystem(PlatformID platformId)
         {
-#if NET5_0_OR_GREATER
-                  return OperatingSystem.IsWindows();
-#else
+            return new System.OperatingSystem(platformId, Environment.OSVersion.Version);
+        }
+        
+        public static bool IsWindows()
+        {
+            return GetSystem(PlatformID.Win32NT).IsWindows();
+        }
+        
+        public static bool IsWindows(this System.OperatingSystem operatingSystem)
+        {
             return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-#endif
         }
 
         // ReSharper disable once InconsistentNaming
-        public static bool IsMacOS(this OperatingSystem operatingSystem)
+        public static bool IsMacOS()
         {
-#if NET5_0_OR_GREATER
-            return OperatingSystem.IsMacOS();
-#else
+            return GetSystem(PlatformID.MacOSX).IsMacOS();
+        }
+        
+        // ReSharper disable once InconsistentNaming
+        public static bool IsMacOS(this System.OperatingSystem operatingSystem)
+        {
             return RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
-#endif
         }
 
-        public static bool IsLinux(this OperatingSystem operatingSystem)
+        public static bool IsLinux()
         {
-#if NET5_0_OR_GREATER
-            return OperatingSystem.IsLinux();
-#else
+            return GetSystem(PlatformID.Unix).IsLinux();
+        }
+        
+        public static bool IsLinux(this System.OperatingSystem operatingSystem)
+        {
             return RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-#endif
         }
 
         // ReSharper disable once InconsistentNaming
-        public static bool IsFreeBSD(this OperatingSystem operatingSystem)
+        public static bool IsFreeBSD()
+        {
+            return GetSystem(PlatformID.Unix).IsFreeBSD();
+        }
+        
+        // ReSharper disable once InconsistentNaming
+        public static bool IsFreeBSD(this System.OperatingSystem operatingSystem)
         {
             return System.Runtime.InteropServices.RuntimeInformation.OSDescription.ToLower().Contains("freebsd");
         }
