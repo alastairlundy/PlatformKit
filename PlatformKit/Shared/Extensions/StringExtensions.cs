1afc37deac46e2adb799cdb659f9c58bfb1e9707
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-04-10 20:21:49 +0100

    backport Dot Counter to PlatformKit 3.6

---
+++ PlatformKit/Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace PlatformKit.Shared.Extensions;
+
+internal static class StringExtensions
+{
+    internal static int CountDotsInString(this string str)
+    {
+        int dotCounter = 0;
+
+        foreach (char c in str)
+        {
+            if (c == '.')
+            {
+                dotCounter++;
+            }
+        }
+
+        return dotCounter;
+    }
+}
