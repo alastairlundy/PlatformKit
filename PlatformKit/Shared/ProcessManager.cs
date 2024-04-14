b3bcbe9ac2fcda1903c0b659e3d1a68e01d74840
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-04-14 08:27:19 +0100

    fix a potential issue

--- PlatformKit/Shared/ProcessManager.cs
+++ PlatformKit/Shared/ProcessManager.cs
@@ -223,7 +223,7 @@ namespace PlatformKit
         {
             if (OSAnalyzer.IsWindows())
             {
-                string location = Environment.SystemDirectory + Path.DirectorySeparatorChar + "System32" + + Path.DirectorySeparatorChar + "WindowsPowerShell" + Path.DirectorySeparatorChar + "v1.0";
+                string location = Environment.SystemDirectory + Path.DirectorySeparatorChar + "System32" + Path.DirectorySeparatorChar + "WindowsPowerShell" + Path.DirectorySeparatorChar + "v1.0";
                 return RunProcessWindows(location, "powershell", command, processStartInfo);
             }
                 
