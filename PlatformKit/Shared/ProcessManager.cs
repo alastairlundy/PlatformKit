de71580b0e86fa81893a3620b4f3b856ba1e163e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-04-22 17:59:25 +0100

    removed unneeded commented out code

--- PlatformKit/Shared/ProcessManager.cs
+++ PlatformKit/Shared/ProcessManager.cs
@@ -106,21 +106,18 @@ namespace PlatformKit
 
                 if (end == null)
                 {
-                    //        PlatformKitAnalytics.ReportError(new NullReferenceException(), nameof(RunProcessWindows));
                     throw new NullReferenceException();
                 }
 
                 if (end.ToLower()
                     .Contains(" is not recognized as the name of a cmdlet, function, script file, or operable program"))
                 {
-                    //        PlatformKitAnalytics.ReportError(new Exception(end), nameof(RunProcessWindows));
                     throw new Exception(end);
                 }
                 else if (end.ToLower()
                          .Contains(
                              "is not recognized as an internal or external command, operable program or batch file."))
                 {
-                    //     PlatformKitAnalytics.ReportError(new Exception(end), nameof(RunProcessWindows));
 
                     throw new Exception(end);
                 }
