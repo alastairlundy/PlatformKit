b012a839f8c214f8cac410019a50c5897108b9f3
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-06-16 21:25:43 +0100

    add spacing

--- PlatformKit/Platforms/Windows/WindowsAnalyzer.cs
+++ PlatformKit/Platforms/Windows/WindowsAnalyzer.cs
@@ -26,8 +26,10 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
+
 using AlastairLundy.Extensions.System.StringExtensions;
 using AlastairLundy.Extensions.System.VersionExtensions;
+
 using PlatformKit.Internal.Deprecation;
 using PlatformKit.Internal.Exceptions;
 
