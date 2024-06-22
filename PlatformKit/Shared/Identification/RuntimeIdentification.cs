f99479d988a7be55a1341290cf93556cbf2e9393
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-06-22 16:56:29 +0100

    add spacing

--- PlatformKit/Shared/Identification/RuntimeIdentification.cs
+++ PlatformKit/Shared/Identification/RuntimeIdentification.cs
@@ -26,6 +26,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
+
 using AlastairLundy.Extensions.System.StringExtensions;
 using PlatformKit.Internal.Exceptions;
 
