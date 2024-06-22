4fac7d2b04cefe2bb9990baa74fa6921eae10fb7
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-06-22 16:13:18 +0100

    deprecate HyperVRequirements and add replacement class

--- PlatformKit/Platforms/Windows/Models/HyperVRequirementsModel.cs
+++ PlatformKit/Platforms/Windows/Models/HyperVRequirementsModel.cs
@@ -24,7 +24,13 @@
 
 namespace PlatformKit.Windows;
 
-public class HyperVRequirements : HyperVRequirementsModel
+public class HyperVRequirementsModel
 {
-   
+    public bool VmMonitorModeExtensions { get; set; }
+    
+    public bool VirtualizationEnabledInFirmware { get; set; }
+    
+    public bool SecondLevelAddressTranslation { get; set; }
+    
+    public bool DataExecutionPreventionAvailable { get; set; }
 }
