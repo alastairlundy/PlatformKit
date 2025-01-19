eb5289b0517e3c914d8545e904c3237a001b30ad
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-01-19 19:40:57 +0000

    add Windows specialization code

---
+++ PlatformKit/Specializations/Windows/WinRegistrySearcher.cs
@@ -0,0 +1,27 @@
+﻿/*
+    PlatformKit
+    Copyright (c) 2020-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System.Threading.Tasks;
+
+using PlatformKit.Specializations.Windows.Abstractions;
+
+namespace PlatformKit.Specializations.Windows;
+
+public class WinRegistrySearcher : IWinRegistrySearcher
+{
+    public async Task<string> GetValueAsync(string query)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<string> GetValueAsync(string query, string key)
+    {
+        throw new System.NotImplementedException();
+    }
+}
