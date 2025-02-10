d06ac85e8b901c44a9a879fc1521e826be454b1d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-10 17:42:59 +0000

    Create CliWrapExecuteSyncExtensions.cs

---
+++ PlatformKit/Internal/Helpers/CliWrapExecuteSyncExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using CliWrap;
+using CliWrap.Buffered;
+
+namespace PlatformKit.Internal.Helpers;
+
+internal static class CliWrapExecuteSyncExtensions
+{
+    internal static BufferedCommandResult ExecuteSync(this Command command)
+    {
+        Task<BufferedCommandResult> task = command.ExecuteBufferedAsync();
+        task.RunSynchronously();
+        
+        task.Wait();
+
+        return task.Result;
+    }
+}
