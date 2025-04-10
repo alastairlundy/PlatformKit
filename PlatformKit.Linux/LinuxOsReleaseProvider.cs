26e2773eb52f58428364c83c4048ba7ed743fa0b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-10 19:54:26 +0100

    update dependencies

--- PlatformKit.Linux/LinuxOsReleaseProvider.cs
+++ PlatformKit.Linux/LinuxOsReleaseProvider.cs
@@ -19,7 +19,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-using AlastairLundy.Extensions.System.Strings;
+using AlastairLundy.DotExtensions.Strings;
 
 using PlatformKit.Linux.Abstractions;
 
