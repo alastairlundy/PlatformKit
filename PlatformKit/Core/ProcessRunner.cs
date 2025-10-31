5715690c6887e1c30fff57c4d95edc4ce2b634de
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-10-31 19:30:12 +0000

    rework ProcessRunner to use CliWrap

--- PlatformKit/Core/ProcessRunner.cs
+++ PlatformKit/Core/ProcessRunner.cs
@@ -26,8 +26,10 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
-
+using CliWrap;
+using CliWrap.Buffered;
 using PlatformKit.Internal.Deprecation;
+using PlatformKit.Internal.Helpers;
 using PlatformKit.Internal.Localizations;
 
 #if NETSTANDARD2_0 || NETSTANDARD2_1
@@ -69,70 +71,12 @@ namespace PlatformKit
                 throw new PlatformNotSupportedException(Resources.Exceptions_PlatformNotSupported_WindowsOnly);
             }
             
-            Process process;
+            BufferedCommandResult result = Cli.Wrap(executableName)
+                .WithArguments(arguments)
+                .WithWorkingDirectory(executableLocation)
+                .ExecuteBufferedSync();
             
-            if (processStartInfo != null)
-            {
-                processStartInfo.WorkingDirectory = executableLocation;
-                processStartInfo.FileName = executableName;
-                processStartInfo.Arguments = arguments;
-                processStartInfo.RedirectStandardOutput = true;
-                process = new Process { StartInfo = processStartInfo};
-            }
-            else
-            {
-                process = new Process();
-                    
-                process.StartInfo.FileName = executableName;
-
-                if (!executableName.EndsWith(".exe") && insertExeInExecutableNameIfMissing)
-                {
-                    process.StartInfo.FileName = $"{process.StartInfo.FileName}.exe";
-                }
-
-                process.StartInfo.WorkingDirectory = executableLocation;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.Arguments = arguments;
-                process.StartInfo.WindowStyle = windowStyle;
-            }
-
-            if (runAsAdministrator)
-            {
-                process.StartInfo.Verb = "runas";
-            }
-
-            if (!Directory.Exists(executableLocation))
-            {
-                throw new DirectoryNotFoundException();
-            }
-
-            Task task = new Task(() => process.Start());
-            task.Start();
-
-            task.Wait();
-            
-            string end = process.StandardOutput.ReadToEnd();
-
-            process.Close();
-            
-            if (end == null)
-            {
-                throw new NullReferenceException();
-            }
-
-            if (end.ToLower()
-                .Contains("is not recognized as the name of a cmdlet, function, script file, or operable program"))
-            {
-                throw new Exception(end);
-            }
-            else if (end.ToLower()
-                     .Contains(
-                         "is not recognized as an internal or external command, operable program or batch file."))
-            {
-                throw new Exception(end);
-            }
-
-            return end;
+            return result.StandardOutput;
         }
 
         /// <summary>
@@ -142,46 +86,20 @@ namespace PlatformKit
         /// <param name="executableName">The name of the file to be run.</param>
         /// <param name="processArguments">Arguments to be passed to the executable.</param>
         [Obsolete(DeprecationMessages.DeprecationV5UseCliRunnerInstead)]
-        public static string RunProcessOnMac(string executableLocation, string executableName, string arguments = "", ProcessStartInfo processStartInfo = null)
+        public static string RunProcessOnMac(string executableLocation, string executableName,
+            string arguments = "", ProcessStartInfo processStartInfo = null)
         {
             if (OperatingSystem.IsMacOS() == false && OperatingSystem.IsMacCatalyst() == false)
             {
                 throw new PlatformNotSupportedException(Resources.Exceptions_PlatformNotSupported_MacOnly);
             }
             
-            ProcessStartInfo procStartInfo = new ProcessStartInfo
-            {
-                WorkingDirectory = executableLocation,
-                FileName = executableName,
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = false,
-                Arguments = arguments
-            };
+            BufferedCommandResult result = Cli.Wrap(executableName)
+                .WithArguments(arguments)
+                .WithWorkingDirectory(executableLocation)
+                .ExecuteBufferedSync();
 
-            Process process;
-            
-            if (processStartInfo == null)
-            {
-                process = new Process { StartInfo = procStartInfo };
-            }
-            else
-            {
-                procStartInfo = processStartInfo;
-                procStartInfo.WorkingDirectory = executableLocation;
-                procStartInfo.FileName = executableName;
-                procStartInfo.Arguments = arguments;
-                procStartInfo.RedirectStandardOutput = true;
-                process = new Process { StartInfo = processStartInfo};
-            }
-            
-            process.Start();
-
-            process.WaitForExit();
-            string output = process.StandardOutput.ReadToEnd();
-
-            process.Close();
-            return output;
+            return result.StandardOutput;
         }
 
         /// <summary>
@@ -193,45 +111,20 @@ namespace PlatformKit
         /// <returns></returns>
         /// <exception cref="Exception"></exception>
         [Obsolete(DeprecationMessages.DeprecationV5UseCliRunnerInstead)]
-        public static string RunProcessOnLinux(string executableLocation, string executableName, string arguments = "", ProcessStartInfo processStartInfo = null)
+        public static string RunProcessOnLinux(string executableLocation, string executableName,
+            string arguments = "", ProcessStartInfo processStartInfo = null)
         {
             if (OperatingSystem.IsLinux() == false && OperatingSystem.IsFreeBSD() == false)
             {
                 throw new PlatformNotSupportedException(Resources.Exceptions_PlatformNotSupported_LinuxOnly);
             }
-            
-            ProcessStartInfo procStartInfo;
-                
-            if (processStartInfo == null)
-            {
-                procStartInfo = new ProcessStartInfo
-                {
-                    WorkingDirectory = executableLocation,
-                    FileName = executableName,
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = false,
-                    Arguments = arguments
-                };
-            }
-            else
-            {
-                procStartInfo = processStartInfo;
-                procStartInfo.WorkingDirectory = executableLocation;
-                procStartInfo.FileName = executableName;
-                procStartInfo.Arguments = arguments;
-                procStartInfo.RedirectStandardOutput = true;
-            }
-                
-            Process process = new Process { StartInfo = procStartInfo };
-            process.Start();
 
-            process.WaitForExit();
+            BufferedCommandResult result = Cli.Wrap(executableName)
+                .WithArguments(arguments)
+                .WithWorkingDirectory(executableLocation)
+                .ExecuteBufferedSync();
             
-            string output = process.StandardOutput.ReadToEnd();
-            
-            process.Close();
-            return output;
+            return result.StandardOutput;
         }
 
         /// <summary>
